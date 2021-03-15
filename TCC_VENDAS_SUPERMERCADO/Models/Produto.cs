0257aa6042160b4e10783232eab34ae9c15359d0
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-03-15 17:23:25 -0300

    Adição de classes principais, ViewModels e view Login

---
+++ TCC_VENDAS_SUPERMERCADO/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCC_VENDAS_SUPERMERCADO.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+
+        //Verificar futuramente se estoque continuara na classe produto
+        public decimal estoque { get; set; }
+    }
+}
---
+++ TCC_VENDAS_SUPERMERCADO/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TCC_VENDAS_SUPERMERCADO.ViewModels
+{
+    public class LoginViewModel
+    {
+        public ICommand EntrarCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            EntrarCommand = new Command(() =>
+            {
+                //    MainPage = new MasterDetailView();
+            });
+        }
+    }
+}
