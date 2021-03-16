5038a594126e573c987bf3111fa5050053053153
Lucas Pedrini <lucasspedrini@gmail.com>
2021-03-16 16:30:17 -0300

    Adição da viewmodel BaseViewModel

---
+++ TCC_VENDAS_SUPERMERCADO/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TCC_VENDAS_SUPERMERCADO.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
