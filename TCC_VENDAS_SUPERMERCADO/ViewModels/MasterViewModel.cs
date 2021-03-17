2a6c519b988964ee949b849a64847ff99f7e9ed1
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-03-17 18:34:33 -0300

    Inclusao da tela de perfil, commands

--- TCC_VENDAS_SUPERMERCADO/ViewModels/MasterViewModel.cs
+++ TCC_VENDAS_SUPERMERCADO/ViewModels/MasterViewModel.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using TCC_VENDAS_SUPERMERCADO.Models;
+using Xamarin.Forms;
 
 namespace TCC_VENDAS_SUPERMERCADO.ViewModels
 {
     public class MasterViewModel : BaseViewModel
     {
+        private readonly Usuario usuario;
+        private ICommand EditarPerfilCommand { get; set; }
+
+        public MasterViewModel(Usuario usuario)
+        {
+            this.usuario = usuario;
+            DefinirComandos(usuario);
+        }
+
+        private void DefinirComandos(Usuario usuario)
+        {
+            EditarPerfilCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(usuario, "EditarPerfil");
+            });
+        }
     }
 }
