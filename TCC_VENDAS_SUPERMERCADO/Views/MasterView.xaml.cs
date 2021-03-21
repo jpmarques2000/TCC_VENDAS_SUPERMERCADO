4b6f3e6a4ae58f7a1dfd7053bf139ee0ca7bc2be
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-03-21 13:53:34 -0300

    Correcao na navegacao(passando usuario para as telas)

--- TCC_VENDAS_SUPERMERCADO/Views/MasterView.xaml.cs
+++ TCC_VENDAS_SUPERMERCADO/Views/MasterView.xaml.cs
@@ -14,26 +14,28 @@ namespace TCC_VENDAS_SUPERMERCADO.Views
     public partial class MasterView : TabbedPage
     {
         public MasterViewModel ViewModel { get; set; }
+        private Usuario _usuario;
         public MasterView(Usuario usuario)
         {
             InitializeComponent();
             this.ViewModel = new MasterViewModel(usuario);
+            _usuario = usuario;
         }
 
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            MessagingCenter.Send<Usuario>(new Usuario(), "EditarPerfil");
+            MessagingCenter.Send<Usuario>(_usuario, "EditarPerfil");
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
         {
-            MessagingCenter.Send<Usuario>(new Usuario(), "MeusPedidos");
+            MessagingCenter.Send<Usuario>(_usuario, "MeusPedidos");
         }
 
         private void Button_Clicked_3(object sender, EventArgs e)
         {
-            MessagingCenter.Send<Usuario>(new Usuario(), "MeuCarrinho");
+            MessagingCenter.Send<Usuario>(_usuario, "MeuCarrinho");
         }
     }
 }
