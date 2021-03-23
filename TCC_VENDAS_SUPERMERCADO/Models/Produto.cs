d7c3830623caa6bf578870cae716cb11aa510c58
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-03-23 17:03:41 -0300

    Carregando produtos a partir de uma lista tela consultaProdutos

--- TCC_VENDAS_SUPERMERCADO/Models/Produto.cs
+++ TCC_VENDAS_SUPERMERCADO/Models/Produto.cs
@@ -8,9 +8,24 @@ namespace TCC_VENDAS_SUPERMERCADO.Models
     {
         public int id { get; set; }
         public string nome { get; set; }
-        public decimal preco { get; set; }
+        public double preco { get; set; }
 
         //Verificar futuramente se estoque continuara na classe produto
         public decimal estoque { get; set; }
+
+        public Produto(int id, string nome, double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+        }
+
+        public string PrecoFormatado
+        {
+            get
+            {
+                return string.Format("Valor: R$ {0}", preco);
+            }
+        }
     }
 }
