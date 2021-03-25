3f763fc3512f53517bf4eba9da6667e4e84d6b09
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-03-25 16:52:24 -0300

    Atualizacao da listas de pedidos

--- TCC_VENDAS_SUPERMERCADO/Views/CarrinhoView.xaml.cs
+++ TCC_VENDAS_SUPERMERCADO/Views/CarrinhoView.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TCC_VENDAS_SUPERMERCADO.Models;
+using TCC_VENDAS_SUPERMERCADO.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,25 @@ namespace TCC_VENDAS_SUPERMERCADO.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CarrinhoView : ContentPage
     {
+        public CarrinhoViewModel ViewModel { get; set; }
         public CarrinhoView()
         {
             InitializeComponent();
+            this.ViewModel = new CarrinhoViewModel();
+            this.BindingContext = this.ViewModel;
+            listViewPedido.ItemsSource = pedidos;
+        }
+
+        List<Pedidos> pedidos = new List<Pedidos>();
+
+        public void carregaProdutos(List<Pedidos> listProdutos)
+        {
+            pedidos = listProdutos;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
